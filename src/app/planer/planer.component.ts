a12701d97cb7e22d91637ccf7daf5364be25d996
grzejatol <grzejatol@gmail.com>
2023-11-23 21:38:23 +0000

    dodanie calego kalendarza

--- src/app/planer/planer.component.ts
+++ src/app/planer/planer.component.ts
@@ -1,11 +1,9 @@
 import {AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild} from '@angular/core';
 import {Database_interface} from "../interface/database_interface";
-import {DateTime} from "luxon";
-import { CdkDragStart} from "@angular/cdk/drag-drop";
+import {DateTime, Interval} from "luxon";
+import {CdkDragStart} from "@angular/cdk/drag-drop";
 import {Job_interface} from "../interface/Job_interface";
 import {saveData} from "../firebase";
-
-
 
 
 @Component({
@@ -15,7 +13,7 @@ import {saveData} from "../firebase";
 
 })
 
-export class PlanerComponent implements AfterViewInit{
+export class PlanerComponent implements AfterViewInit {
   // @ViewChild('test') testDiv!: ElementRef;
   jobList: Job_interface[] = [];
   jobListFilter: Job_interface[] = [];
@@ -23,13 +21,14 @@ export class PlanerComponent implements AfterViewInit{
   dataOd: string = '2023-11-05';
   dataDo: string = '2023-11-10';
   jobHours: number = 0;
-  hours: number[] =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+  hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
   topPosition: number = 20;
   leftPosition: number = 0;
   mousePositionX: number = 0;
   mousePositionY: number = 0;
-  elementOffset: {left:number,top:number } = {left:0,top:0};
+  elementOffset: { left: number, top: number } = {left: 0, top: 0};
   filtr: Database_interface[] = [];
+  daysInRange: any;
   dataBase: Database_interface[] = [{
     name: 'Zadanie 1',
     data: '2023-11-01',
@@ -105,7 +104,7 @@ export class PlanerComponent implements AfterViewInit{
   }
 
   ];
-  jobName: string ='';
+  jobName: string = '';
   jobDescription: string = '';
   jobDate: any;
   jobTime: any;
@@ -113,17 +112,17 @@ export class PlanerComponent implements AfterViewInit{
   jobPositionY: number = 0;
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {
-   this.jobList = [{
+    this.jobList = [{
       id: 1,
       job_name: 'Zadanie 1',
       job_description: 'Zadanie 1',
-     job_date: '2023-11-05',
+      job_date: '2023-11-05',
       job_hours: 8,
       begin_time: 17,
       job_Position_X: 700,
       job_Position_Y: 0
     }, {
-      id: 2 ,
+      id: 2,
       job_name: 'Zadanie 2',
       job_description: 'Zadanie 2',
       job_date: '2023-11-06',
@@ -132,23 +131,43 @@ export class PlanerComponent implements AfterViewInit{
       job_Position_X: 0,
       job_Position_Y: 0
     }];
-   this.onDateChange();
-   }
-ngAfterViewInit() {
+    this.onDateChange();
+  }
 
-}
+  ngAfterViewInit() {
+
+  }
+
+// odczytuje kalendarz i wczytuje wszystkie zadania na te dni
   onDateChange() {
-    this.filtr = this.dataBase.filter((item) => {
-      return item.data >= this.dataOd && item.data <= this.dataDo;
-    });
-    this.jobListFilter = this.jobList.filter((item) => {
+    const od = DateTime.fromISO(this.dataOd);
+    const doo = DateTime.fromISO(this.dataDo);
+    const dateInterval = Interval.fromDateTimes(od, doo);
+    console.log(dateInterval.count('days'));
+    this.daysInRange = dateInterval.splitBy({days: 1}).map((item) => item.start);
+    console.log(this.daysInRange);
+    console.log(this.daysInRange[0].toFormat('yyyy-MM-dd'));
+     this.jobListFilter = this.jobList.filter((item) => {
       return item.job_date >= this.dataOd && item.job_date <= this.dataDo;
-    });
+     });
     this.jobListFilter.forEach((item) => {
-      item.job_Position_X = 10 * item.begin_time + ((this.daysDifference(this.dataOd, item.job_date))! * 240) ;
+      item.job_Position_X = 10 * item.begin_time + ((this.daysDifference(this.dataOd, item.job_date))! * 240);
 
     });
   }
+  // onDateChange() {
+  //   this.filtr = this.dataBase.filter((item) => {
+  //     return item.data >= this.dataOd && item.data <= this.dataDo;
+  //   });
+  //   this.jobListFilter = this.jobList.filter((item) => {
+  //     return item.job_date >= this.dataOd && item.job_date <= this.dataDo;
+  //   });
+  //   this.jobListFilter.forEach((item) => {
+  //     item.job_Position_X = 10 * item.begin_time + ((this.daysDifference(this.dataOd, item.job_date))! * 240);
+  //
+  //   });
+  // }
+
   //obliczam roznicę w dniach miedzy datami
   daysDifference(date1: string, date2: string) {
     const date1Obj = DateTime.fromISO(date1);
@@ -158,6 +177,9 @@ ngAfterViewInit() {
     return Math.abs(diff.days!);
   }
 
+  //przeliczam i zapisuje kazde zadanie na odpowiednia pozycje
+  //dodaje animacje do kazdego zadania
+  //glowny event do przesuwania zadania
   changeJobPosition(job: Job_interface, event: any) {
 
     const element = event.target as HTMLElement;
@@ -167,31 +189,28 @@ ngAfterViewInit() {
     const newX = this.leftPosition - (this.leftPosition % 10);
     console.log('newX' + newX);
     const newJobListPos: Job_interface[] = [];
-     this.jobList.forEach(item => {
-       if(item.id !== thisJob!.id) {
-         if (item.job_Position_X <= newX + 79 && item.job_Position_X + 79 >= newX) {
-           newJobListPos.push(item);
-           console.log('newJobListPos' + newJobListPos.length);
-
-
-
-         }
-       }
-
-
+    this.jobList.forEach(item => {
+      if (item.id !== thisJob!.id) {
+        if (item.job_Position_X <= newX + ((thisJob!.job_hours * 10) - 1) && item.job_Position_X + ((item.job_hours * 10) - 1) >= newX) {
+          newJobListPos.push(item);
+          console.log('newJobListPos' + newJobListPos.length);
+        }
+      }
     });
-
-
-     if(newJobListPos.length === 0) {
-       thisJob!.job_Position_X = newX;
-       element.style.opacity = '1';
-       element.style.boxShadow = 'none';
-     } else {
-    //
-         element.style.opacity = '1';
-        element.style.boxShadow = 'none';
-    //
-     }
+    if (newJobListPos.length === 0) {
+      thisJob!.job_Position_X = newX;
+      element.style.opacity = '1';
+      element.style.boxShadow = 'none';
+      const claculateDay = Math.floor(newX / 240);
+      const date = DateTime.fromISO(this.dataOd).plus({days: claculateDay});
+      thisJob!.job_date = date.toFormat('yyyy-MM-dd');
+      thisJob!.begin_time = newX % 240 / 10;
+    } else {
+      //
+      element.style.opacity = '1';
+      element.style.boxShadow = 'none';
+      //
+    }
 
     console.log(this.topPosition + ' ' + this.leftPosition);
 
@@ -205,11 +224,6 @@ ngAfterViewInit() {
   // }
 
 
-
-
-
-
-
   // tu pobiera pozycje myszki
   @HostListener('mousemove', ['$event'])
   onMouseMove(event: any) {
@@ -220,6 +234,7 @@ ngAfterViewInit() {
       this.mousePositionY = event.clientY;
     }
   }
+
   // tu ustawiam offset dla elementu ktory chce przesunac wzgledem pozycji myszki
 
   getOffset(el: any) {
@@ -238,7 +253,7 @@ ngAfterViewInit() {
     const newJob: Job_interface = {
       id: this.jobList.length + 1,
       job_name: this.jobName,
-      job_date:  DateTime.now().toFormat('yyyy-MM-dd'),
+      job_date: DateTime.now().toFormat('yyyy-MM-dd'),
       job_hours: this.jobHours,
       begin_time: 0,
       job_description: this.jobDescription,
